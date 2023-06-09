a1ac9d422b6f5929eb06246d8432c1579d45d590
Taron <tar8117@yandex.ru>
2023-06-09 11:57:37 +0600

    v1

---
+++ app_main/exchange/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import requests
+
+
+def exchange(request):
+    response = requests.get(url='https://v6.exchangerate-api.com/v6/c328a5b60108c6f0df500352/latest/USD').json()
+    currencies = response.get('conversion_rates')
+
+    if request.method == 'GET':
+        context = {
+            'currencies': currencies
+        }
+
+        return render(request=request, template_name='exchange/index.html', context=context)
+
+    if request.method == 'POST':
+        from_amount = float(request.POST.get('from-amount'))
+        from_curr = request.POST.get('from-curr')
+        to_curr = request.POST.get('to-curr')
+        res = (currencies[to_curr] / currencies[from_curr]) * float(from_amount)
+
+        converted_amount = round(res, 2)
+
+        context = {
+            'from_curr': from_curr,
+            'to_curr': to_curr,
+            'from_amount': from_amount,
+            'currencies': currencies,
+            'converted_amount': converted_amount
+        }
+
+        return render(request=request, template_name='exchange/index.html', context=context)
